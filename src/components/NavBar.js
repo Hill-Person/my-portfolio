26464c1f4ce427fcd7fa78f2b88d8febd1a012a8
Hill-Person <barkerty@gmail.com>
2020-12-18 16:43:38 -0500

    workin' on the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,16 +7,18 @@ export default function Navbar() {
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
 
-          <NavLink to="/" exact>
+          <NavLink to="/" exact
+          activeClassName="text-gray"
+          className="inflex-flex items-center py-6 px-3 mr-4 text-green-100 hover:text-gray-800 text-4xl cursive tracking-widest">
             Ty
           </NavLink>
-          <NavLink to="/post">
+          <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800">
             Blog Posts
           </NavLink>
-          <NavLink to="/project">
+          <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800">
             Projects
           </NavLink>
-          <NavLink to="/about">
+          <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800">
             About Me!
           </NavLink>
 
