465cdec8e6494a2d4332e3a53938c39f9cc97a62
Hill-Person <barkerty@gmail.com>
2020-12-18 19:27:55 -0500

    lil guy

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
+// import { SocialIcon } from "react-social-icons"
 
 export default function Navbar() {
   return (
@@ -9,20 +10,30 @@ export default function Navbar() {
 
           <NavLink to="/" exact
           activeClassName="text-gray"
-          className="inflex-flex items-center py-6 px-3 mr-4 text-green-100 hover:text-gray-800 text-4xl cursive tracking-widest">
-            Ty
+          className="inflex-flex items-center py-3 px-6 mr-4 text-green-100 hover:text-gray-800 text-4xl cursive tracking-widest">
+            Ty Barker
           </NavLink>
-          <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800">
+          <NavLink to="/post"
+          className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800"
+          activeClassName="text-green-100 bg-green-700"
+          >
             Blog Posts
           </NavLink>
-          <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800">
+          <NavLink to="/project"
+          className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800"
+          activeClassName="text-green-100 bg-green-700"
+          >
             Projects
           </NavLink>
-          <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800">
+          <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-green-200 hover:text-gray-800"
+          activeClassName="text-green-100 bg-green-700"
+          >
             About Me!
           </NavLink>
-
         </nav>
+        <div>
+
+        </div>
       </div>
     </header>
   )
